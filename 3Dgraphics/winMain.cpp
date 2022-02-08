af4d732a9eb095c32c599540a585e5aa1256e552
Arseniy <ambelyakov@gmail.com>
2022-02-08 21:40:23 +0300

    affine transformations ready

---
+++ 3Dgraphics/winMain.cpp
@@ -0,0 +1,298 @@
+#include <stdexcept>
+#include <cmath>
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
+#include <winbase.h>
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+#define PI 3.14159265
+#define width 1080
+#define height 720
+
+using namespace std;
+
+class affinsMatrix2D {
+private:
+	float matrix[3][3];
+	float** retMatrix;
+	void reloadMatrix() {
+		for (int i = 0; i < 3; i++ ) {
+			for (int j = 0; j < 3; j++ ) {
+				if (i != j) { matrix[i][j] = 0; }
+				else { matrix[i][j] = 1; }
+			}
+		}
+	}
+	float degreeToRad(float degree) {
+		return degree * PI / 180;
+	}
+	void placeHolder() {
+		retMatrix = (float**)malloc(sizeof(float*) * 3);
+		for (int i = 0; i < 3;i++) {
+			retMatrix[i] = (float*)malloc(sizeof(float) * 3);
+		}
+	}
+public:
+	affinsMatrix2D(){
+		this->reloadMatrix();
+		this->placeHolder();
+	}
+
+	void rotate(float angle) {
+		this->reloadMatrix();
+		angle = degreeToRad(angle);
+		matrix[0][0] = cos(angle);
+		matrix[1][1] = cos(angle);
+		matrix[0][1] = sin(angle);
+		matrix[1][0] = -sin(angle);
+	}
+	void move_on(int x = 0, int y = 0) {
+		this->reloadMatrix();
+		matrix[0][2] = x;
+		matrix[1][2] = y;
+	}
+	void scaling(float multuX = 1, float multuY = 1) {
+		this->reloadMatrix();
+		matrix[0][0] = 1/multuX;
+		matrix[1][1] = 1/multuY;
+	}
+	float** getMatrix() {
+		for (int i = 0; i<3; i++) {
+			for (int j = 0; j<3; j++) {retMatrix[i][j] = matrix[i][j];}
+		}
+		return retMatrix;
+	}
+};
+
+class affinsMatrix3D {
+private:
+	float matrix[4][4];
+public:
+	void rotate(float angle) {
+
+	}
+	void move_on(int x, int y, int z) {
+
+	}
+	void augmentation(float multu) {
+
+	}
+};
+
+class point2D {
+private:
+	float x, y, z;
+public:
+	point2D(float x, float y) {
+		this->x = x;
+		this->y = y;
+		this->z = 1;
+	}
+	point2D() {
+		this->x = 1;
+		this->y = 1;
+		this->z = 1;
+	}
+	float get(int i) {
+		switch (i)
+		{
+		case 0:
+			return x;
+			
+		case 1:
+			return y;
+		case 2:
+			return z;
+		default:
+			break;
+		}
+	}
+	void set(int i, float arg) {
+		switch (i)
+		{
+		case 0:
+			x = arg;
+
+		case 1:
+			y = arg;
+		default:
+			break;
+		}
+	}
+};
+
+class point3D {
+private:
+	float x, y, z, w;
+public:
+	point3D(float x, float y, float z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->w = 1;
+	}
+};
+
+class point2DArray{
+private:
+	point2D* pointArray;
+	int realSize;
+	int maxSize;
+public:
+	point2DArray(int size) {
+		this->maxSize = size;
+		this->realSize = 0;
+		pointArray = (point2D*)malloc(maxSize * sizeof(point2D));
+	}
+	point2DArray operator *(affinsMatrix2D inputMatrix) {
+		point2DArray result = point2DArray(maxSize);
+		float** matrix = inputMatrix.getMatrix();
+
+		for (int i = 0;i < realSize;i++) {
+			point2D localPoint;
+			for (int j = 0;j < 3;j++) {
+				float localResult = 0;
+				for (int k = 0;k < 3;k++) {
+					localResult += pointArray[i].get(k) * matrix[j][k];
+				}
+				localPoint.set(j, localResult);
+			}
+			result.add(localPoint);
+		}
+		return result;
+	}
+	void add(point2D point) { //переделать
+		if (maxSize == realSize) {
+			throw invalid_argument("array overload");
+		}
+		else {
+			pointArray[realSize] = point;
+			realSize++;
+		}
+	}
+	point2D getPoint(int i) {
+		return pointArray[i];
+	}
+	int getSize() {
+		return realSize;
+	}
+};
+
+class point3DArray{
+private:
+	point3D* pointArray;
+public:
+	point3DArray Multiplication() {
+
+	}
+};
+
+VOID OnPaint(HDC hdc,point2D startPoint, point2D endPoint)
+{
+	Graphics graphics(hdc);
+	Pen      pen(Color(255, 0, 0, 0));
+	graphics.DrawLine(&pen, startPoint.get(0), startPoint.get(1), endPoint.get(0), endPoint.get(1));
+}
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+point2DArray picture = point2DArray(15);
+int path[23] = { 0,1,0,2,0,3,4,3,5,3,6,7,6,8,6,9,10,9,11,9,12,13,14 };
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
+{
+	affinsMatrix2D affins;
+	picture.add(point2D(width /2,100));
+	picture.add(point2D(width /2-25,125));
+	picture.add(point2D(width /2+25,125));
+	
+	picture.add(point2D(width / 2, 125));
+	picture.add(point2D(width / 2 - 50, 175));
+	picture.add(point2D(width / 2 + 50, 175));
+
+	picture.add(point2D(width / 2, 175));
+	picture.add(point2D(width / 2 - 75, 250));
+	picture.add(point2D(width / 2 + 75, 250));
+
+	picture.add(point2D(width / 2, 250));
+	picture.add(point2D(width / 2 - 100, 350));
+	picture.add(point2D(width / 2 + 100, 350));
+
+	picture.add(point2D(width / 2, 400));
+	picture.add(point2D(width / 2 - 50, 400));
+	picture.add(point2D(width / 2 + 50, 400));
+
+	affins.scaling(2,2);
+
+	picture = picture * affins;
+
+	HWND                hWnd;
+	MSG                 msg;
+	WNDCLASS            wndClass;
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+
+	// Initialize GDI+.
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = TEXT("GettingStarted");
+
+	RegisterClass(&wndClass);
+
+	hWnd = CreateWindow(
+		TEXT("GettingStarted"),   // window class name
+		TEXT("Getting Started"),  // window caption
+		WS_OVERLAPPEDWINDOW,      // window style
+		CW_USEDEFAULT,            // initial x position
+		CW_USEDEFAULT,            // initial y position
+		width,            // initial x size
+		height,            // initial y size
+		NULL,                     // parent window handle
+		NULL,                     // window menu handle
+		hInstance,                // program instance handle
+		NULL);                    // creation parameters
+
+	ShowWindow(hWnd, iCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	GdiplusShutdown(gdiplusToken);
+	return msg.wParam;
+}  // WinMain
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC          hdc;
+	PAINTSTRUCT  ps;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		for (int i = 0; i < 22; i++) {
+ 			OnPaint(hdc,picture.getPoint(path[i]),picture.getPoint(path[i+1]));
+		}
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+} // WndProc
